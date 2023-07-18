ce4bdab4da21cf030df653537fc9e3f6c22f54f0
Damix <96869024+Damix2131@users.noreply.github.com>
2023-07-18 17:08:06 +0200

    Update AutoEffectFarm.lua

--- LPI-LAI/AutoEffectFarm.lua
+++ LPI-LAI/AutoEffectFarm.lua
@@ -1,9 +1,15 @@
+local Player = game:GetService("Players").LocalPlayer
+local Character = Player.Character
+local Humanoid = Character.Humanoid
+local Root = Character.HumanoidRootPart
+local PlayerGui = Player.PlayerGui
+local AutoKill = false -- turn on if you want to keep dying constantly
+local Method = 1 -- 1. Set Humanoid To 0, 2. Teleport To Void
+
 while true do
     wait()
     pcall(
         function()
-            local Player = game:GetService("Players").LocalPlayer
-            local PlayerGui = Player.PlayerGui
             local CoreGui = PlayerGui:WaitForChild("CoreGui")
             local Main = CoreGui.InGameShop.Main
             local Check1 = Main.Guide.TextLabel
@@ -25,6 +31,19 @@ while true do
                 RollEffect:InvokeServer("Roll")
                 if CoreGui.InGameShop.RollButton.TextLabel.Text ~= Text3 then
                     print(CoreGui.InGameShop.RollButton.TextLabel.Text .. " || Script Made By Damix :3")
+                end
+            elseif Player.leaderstats.Fame.Value <= 10 then
+                if AutoKill then
+                    if Check1.Text ~= Text1 or Check1.Text ~= Text2 then
+                        if Humanoid.Health ~= 0 then
+                            if Method == 1 then
+                                Humanoid.Health = 0
+                            end
+                            if Method == 2 then
+                                Root.CFrame = CFrame.new(0, -1000, 0)
+                            end
+                        end
+                    end
                 end
             end
         end
